868b74819bc8e50281feafcb8ce9144ff3afad7b
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-28 03:21:28 +0500

    Task2

---
+++ Tyuiu.DanilovAS.Sprint3.Task2.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.DanilovAS.Sprint3.Task2.V21.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint3.Task2.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Данилов А. С. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла do - while                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #21                                                              *");
+            Console.WriteLine("* Выполнил: Данилов Александр Сергеевич |  ИИПб-24-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+            Console.WriteLine("* ряда по формуле, при х=1,5                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*									      *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 1.5;
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Сумма ряда равна = " + ds.GetSumSeries(x,startValue,stopValue));
+            Console.ReadKey();
+        }
+    }
+}
