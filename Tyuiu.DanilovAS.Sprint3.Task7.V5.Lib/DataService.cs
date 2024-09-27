3bd7a6a91d0fcd39972feec34443c03d6512d17e
Tyuiu.DanilovAlexander <Tyuiu.DanilovAlexander@outlook.com>
2024-09-28 04:51:29 +0500

    Task7

---
+++ Tyuiu.DanilovAS.Sprint3.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DanilovAS.Sprint3.Task7.V5.Lib
+{
+    public class DataService : ISprint3Task7V5
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+
+            double y;
+            int count = 0;
+            //y =(2*Math.Sin(i))/(3*i+1.2) +Math.Cos(i) - 7*i*2;
+            //y = (2*(Math.Sin(i)))/(3*i+1.2) + Math.Cos(i) - 7* i *2;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                y = (2 * Math.Sin(i)) / (3 * i + 1.2) + Math.Cos(i) - 7 * i * 2;
+                valueArray[count] = y;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint3.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,39 @@
+using Tyuiu.DanilovAS.Sprint3.Task7.V5.Lib;
+
+namespace Tyuiu.DanilovAS.Sprint3.Task7.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestGetMassFunction()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -69.83 ;
+            valueWaitArray[1] = 0.00;
+            valueWaitArray[2] = 0.00;
+            valueWaitArray[3] = 0.00;
+            valueWaitArray[4] = 0.00;
+            valueWaitArray[5] = 0.00;
+            valueWaitArray[6] = 0.00;
+            valueWaitArray[7] = 0.00;
+            valueWaitArray[8] = 0.00;
+            valueWaitArray[9] = 0.00;
+            valueWaitArray[10] = 0.00;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(valueWaitArray,res);
+        }
+    }
+}
